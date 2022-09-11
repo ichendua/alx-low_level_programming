7579b40431c742790511905b695a21440f64f1f7
ichendua <achinewhu@outlook.com>
2022-09-11 19:56:48 +0100

    Hello code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - print out the function
+ * main - print the string in the put function
  *
  * Description: using the main function
  * this program print "Programming is like building a multilingual puzzle

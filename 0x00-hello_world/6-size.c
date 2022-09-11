9bb2dc26bb3a2ece9e397f8f970ab9d3a97754bd
ichendua <achinewhu@outlook.com>
2022-09-12 00:06:45 +0100

    Coding size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - String entry point
+ * main - Entry point
  *
  * Description: using the main function
  * Return: 0
@@ -13,10 +13,10 @@ int main (void)
 	long long lli;
 	float f;
 
-	printf("Size of a char: %ld byte(s)\n", size of (c));
-	printf("Size of an int: %ld byte(s)\n", size of (i));
-	printf("Size of a long int: %ld byte(s)\n", size of (li));
-	printf("Size of a long long int: %ld byte(s)\n", size of (lli));
-	printf("Size of a float: %1d byte(s)\n", size of (f));
+	printf("Size of a char: %ld byte(s)\n", sizeof(c));
+	printf("Size of an int: %ld byte(s)\n", sizeof(i));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(li));
+	printf("Size of a long long int: %ld byte(s)\n", sizeof(lli));
+	printf("Size of a float: %1d byte(s)\n", sizeof(f));
 	return (0);
 }

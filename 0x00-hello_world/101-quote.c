ce897d67c2dddefcbfc164709635079e5beaa855
ichendua <achinewhu@outlook.com>
2022-09-11 23:38:11 +0100

    UNIX is basically

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 1 (Success)
+ */
+int main(void)
+{
+	char str1[] = "and that piece of art is useful\" - Global Access,2019-01-12\n"
+
+	write(2, strl, 59);
+	return (1);
+}

e25e67391c76fa83bebdc75b24123d98d85f8628
ichendua <achinewhu@outlook.com>
2022-09-12 16:54:44 +0100

    PrintCode

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,21 +1,19 @@
 #include <stdio.h>
 
 /**
- * main- program to print alphabet
- * in lower case
- *
- * Return: Always 0 (Success)
+ *  main - Prints the alphabet.
+ *  
+ *  Return: Always 0 (Success)
  */
-int main(void) 
+int main(void)
 {
-	char alp[26] = "az"
-	int i;
+		char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+		int i;
 
-	for (int i = 97; 1 <=122; i++)
-	{
-
-		putchar(alp[i]);
-	}
-	putchar('\n');
-	return (0);
+		for (i = 0; i < 26; i++)
+		{
+			putchar(alp[i]);
+		}
+		putchar('\n');
+		return (0);
 }

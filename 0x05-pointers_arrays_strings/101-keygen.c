2a9655fb8fea44f72d33208c1e6010591faa4f6b
ichendua <achinewhu@outlook.com>
2022-09-20 16:39:09 +0100

    Keygen

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -15,12 +15,12 @@ int main(void)
 	srand((unsigned int) time(&t));
 	while (c < 2772)
 	{
-		r = rand() % 128;
+		r = (rand() % 128);
 		if ((c + r) > 2772)
 			break;
 		c = c + r;
 		printf("%c", r);
 	}
 	printf("%c\n", (2772 - c));
-	return (0);
+i	return (0);
 }

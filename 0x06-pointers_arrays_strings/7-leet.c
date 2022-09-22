e8eedc1cf9c905c75af101e836e15e513cbe04e7
ichendua <achinewhu@outlook.com>
2022-09-22 13:06:13 +0100

    7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,6 +1,7 @@
 #include "main.h"
 
-/** leet - encodes a string into 1337
+/**
+ * leet - encodes a string into 1337
  * @s: string to encode
  *
  * Return: address of s
@@ -15,10 +16,9 @@ char *leet(char *s)
 	{
 		for (j = 0; j <= 9; j++)
 		{
-		if (a[j] == *(s + i))
-			*(s + i) = b[j];
+			if (a[j] == *(s + i))
+				*(s + i) = b[j];
 		}
 	}
 	return (s);
 }
-

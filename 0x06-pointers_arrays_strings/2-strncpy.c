cbdf33cc2e6141ea6879f105e389e52366051160
ichendua <achinewhu@outlook.com>
2022-09-22 01:23:45 +0100

    2-strncpy.c

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strncat - function that copies two strings using at most
+ * @dest: The buffer string to store the string copy
+ * @src: The string source
+ * @n: The maximum number of bytes to copied from src.
+ *
+ * Return: A pointer to the resulting string dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+       	int index = 0, src_len = 0;
+
+	while (src[index++])
+		src_len++;
+	for (index = 0; src[index] && index < n; index++)
+		dest[index] = src[index];
+
+	for (index = src_len; index < n; index++)
+		dest[index] = '\0';
+
+	return (dest);
+}

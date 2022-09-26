73bf65f428a10b7f13db7885be9e02977f9e3861
ichendua <achinewhu@outlook.com>
2022-09-26 13:37:49 +0100

    0-memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,26 @@
+/*
+ * File: 0-memset.c
+ * Auth: not
+ */
+
+#include "main.h"
+/**
+* _memset - fills the first n bytes of the memory area
+* @s: Apointer to the memory area to be filled.
+* @c: The character to fill the memory area with.
+* @n: The number of bytes to be filled.
+* Description _memset: over there
+*
+* Return: A pointer to be filled memory area @s
+*
+*/
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int index;
+	unsigned char *memory = s, value = c;
+
+	for (index = 0; index < n; index++)
+		memory[index] = value
+	
+	return (memory);
+}

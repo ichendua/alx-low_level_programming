473127fc281dba5ab5b04b53aa5563c86b706ee6
ichendua <achinewhu@outlook.com>
2022-09-27 11:55:21 +0100

    memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,20 +1,20 @@
 #include "main.h"
 
 /**
- * _memset
+ * _memset - fills memory with a constant byte
  * @s: address to memory block
  * @b: char to be used
  * @n: number of bytes to be used
  *
- * Return: pointer to memory block
+ * Return: pointer to dest
  */
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	unsigned int a;
-	for (a = 0; a < n; a++)
-	{
-		s[a] = b;
-	}
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		*(s + i) = b;
+
 	return (s);
 }

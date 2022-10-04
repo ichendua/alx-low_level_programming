67425e0d069b4d5b039e60b102a5b5d1c4dc11cd
ichendua <achinewhu@outlook.com>
2022-10-04 15:28:14 +0100

    Satic library

---
+++ 0x09-static_libraries/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _memset - fills memory with a constant byte
+ * @s: address to memory block
+ * @b: char to be used
+ * @n: number of bytes to be used
+ *
+ * Return: pointer to dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		*(s + i) = b;
+
+	return (s);
+}

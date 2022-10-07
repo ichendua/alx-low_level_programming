c7896084dc4989de344c48417ffa7292d1d170e8
ichendua <achinewhu@outlook.com>
2022-10-07 12:28:28 +0100

    Task Calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,5 +1,3 @@
-#include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
 /**
  * _calloc - allocates memory for an initialized buffer
@@ -9,19 +7,16 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	char *p;
+	int *p;
 	unsigned int i;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
-
 	p = malloc(size * nmemb);
-
 	if (p == NULL)
 		return (NULL);
 
-	for (i = 0; i < nmemb * size); i++)
+	for (i = 0; i < nmemb ; i++)
 		p[i] = 0;
-
 	return (p);
 }

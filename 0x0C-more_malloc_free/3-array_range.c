a3b44a6b9e393f4fe198d7267c3a8c8f493780f1
ichendua <achinewhu@outlook.com>
2022-10-07 14:25:48 +0100

    Task 3 Update

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -14,12 +14,12 @@ int *array_range(int min, int max)
 {
 	int *ptr;
 	int arr;
-	
+
 	if (min > max)
 	{
 		return (NULL);
 	}
-	
+
 	ptr = malloc(sizeof(int) * (max - min + 1));
 	if (ptr == NULL)
 	{

bc82b5c1cc2f02f97d88593507329f09a8277260
ichendua <achinewhu@outlook.com>
2022-10-13 08:44:41 +0100

    Task0Print name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,10 +1,12 @@
-#include "function_pointers.h"
 /**
  * print_name - prints a name
  * @name: input string
  * @f: function pointer casted to char pointer
+ *
+ * Return: void.
  */
 void print_name(char *name, void (*f)(char *))
 {
-	(*f)(name);
+	if (name && f)
+		f(name);
 }
--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,23 +1,27 @@
-#include "function_pointers.h"
 #include <stdio.h>
+#include "function_pointers.h"
 /**
- * int_index - searches for an integer
- * @array: pointer to the array
- * @size: size of the array
- * @cmp: pointer to the function
- * Return: index of the first element found
+ * int_index - function to return index of compared int
+ * @array: pointer to array of int type
+ * @size: size of array of int type
+ * @cmp: pointer to function of int type
+ *
+ * Return: return index number of comp int
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	int i;
+	int ind;
 
-	i = -1;
-	if (size <= 0 || !(array) || !(cmp))
+	if (array == NULL)
 		return (-1);
-	while (++i < size)
+	if (size <= 0)
+		return (-1);
+	if (cmp == NULL)
+		return (-1);
+	for (ind = 0; ind < size; ind++)
 	{
-		if (cmp(array[i]))
-			return (i);
+		if (cmp(array[ind]))
+			return (ind);
 	}
 	return (-1);
 }

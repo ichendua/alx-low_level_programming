f418cdf865be8c4b79734f766058a596bb3c498b
ichendua <achinewhu@outlook.com>
2022-10-14 11:44:31 +0100

    Task3

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -6,7 +6,6 @@
  */
 int (*get_op_func(char *s))(int, int)
 {
-	int i;
 	op_t ops[] = {
 		{"+", op_add},
 		{"-", op_sub},
@@ -15,11 +14,15 @@ int (*get_op_func(char *s))(int, int)
 		{"%", op_mod},
 		{NULL, NULL}
 	};
+	int i;
+
 	i = 0;
 	while (i < 5)
 	{
-		if (*ops[i].op == *s && !(*(s + 1)))
+		if (s[0] == ops[i].op[0])
+		{
 			return (ops[i].f);
+		}
 		i++;
 	}
 	return (NULL);
